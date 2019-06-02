51b01109f9a31c1ac2ed4721fb489708ea0b201a
Sourav Padhiari <padhiarisourav@gmail.com>
2019-06-03 02:22:41 +0530

    1

--- KanbanBoard/urls.py
+++ KanbanBoard/urls.py
@@ -14,10 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, url 
-from kanban.views import index
+from django.urls import path 
+from kanban import views 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    url(r'^$', 'index', name='index'),
+    path('',views.index)
+    
 ]
--- kanban/views.py
+++ kanban/views.py
@@ -3,7 +3,6 @@ from kanban.models import Issues
 # Create your views here.
 
 def index(request):
-    iss = Issues.objects.get.all()
     if request.method == "POST":
         if "add" in request.POST:
             desc = request.POST["desc"]
@@ -16,4 +15,4 @@ def index(request):
             bklog = Issues.objects.filter(id= desc)
             bklog.delete()
 
-    return render(request, "index.html", iss )
+    return render(request, 'index.html' )
